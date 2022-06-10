83af9958aa880552d15b613ef4d4e235b03cec3c
Sanoop <sanoop.k@teksalah.com>
2022-06-10 23:47:25 +0530

    Cassandra

---
+++ src/test/java/com/example/caasandra/springcassandraintegration/SpringCassandraIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caasandra.springcassandraintegration;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCassandraIntegrationApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
